e7b686636d41f7fd9b135f49204da5a82b0b245f
luca-avila <avilaluca61@gmail.com>
2025-08-23 14:11:22 -0300

    Implement transactions actions structure

---
+++ src/api/transactions.js
@@ -0,0 +1,19 @@
+import { API_CONFIG } from './config.js';
+
+export class TransactionService {
+    static async getAll() {
+        // GET /transactions
+    }
+
+    static async create(transaction) {
+        // POST /transactions
+    }
+
+    static async update(id, transaction) {
+        // PUT /transactions/:id
+    }
+
+    static async delete(id) {
+        // DELETE /transactions/:id
+    }
+}
